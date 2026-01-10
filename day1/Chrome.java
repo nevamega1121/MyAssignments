43ca26590841c3a6163a320df587363655b62d8d
nevamega1121 <nevamega1121@gmail.com>
2026-01-10 06:35:30 +0530

    Add files via upload
    Week1.Day1

---
+++ day1/Chrome.java
@@ -0,0 +1,15 @@
+package org.myassignemnts.week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer ="Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey ='C';
+		System.out.println("The Browser name is"+developer+" and the Release YEar is "+releaseYear+" and the version is "+version+" and it is "+isBeta+" that it is Beta With schortcut as "+shortcutKey);
+
+	}
+
+}
